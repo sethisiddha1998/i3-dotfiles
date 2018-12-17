1b74de6ada33da3f0e5fec4f9cc882caef6fdb6c
Sean <sean_ling@kiipo.com>
2018-12-17 13:37:16 +0800

    solarc color

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,7 +10,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar top &
 polybar bottom &
 
-if xrandr | grep "HDMI-1-2 connected"
+if xrandr | grep "HDMI-0 connected"
 then polybar top_secondary &
   polybar bottom_secondary &
 fi
